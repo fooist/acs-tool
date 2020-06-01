9c268e09d46cd3cee5be5c9eabdfae991fdfb1f8
lathropd <lathropd@arclight.dreamhost.com>
2020-06-01 12:30:37 -0700

    almost working version of download with params, but curl --head times out

--- download_acs.sh
+++ download_acs.sh
@@ -2,25 +2,56 @@
 
 # make sure the directory I want is there
 # should throw an error and quit if it doesn't work
-mkdir -p data
-mkdir -p data/raw
-mkdir -p data/raw/acs
-mkdir -p data/raw/acs/2018_1_yr
-mkdir -p data/raw/acs/2018_1_yr/data
-mkdir -p data/raw/acs/2018_1_yr/templates
+mkdir -p data/raw/acs/$1_$2_yr/data
+mkdir -p data/raw/acs/$1_$2_yr/templates
 
 # create all the output directories I'm gonna need
-mkdir -p data/processed
-mkdir -p data/processed/acs
-mkdir -p data/processed/acs/2018_1_yr
-mkdir -p data/processed/acs/2018_1_yr/headers
-mkdir -p data/processed/acs/2018_1_yr/combined
-mkdir -p data/processed/acs/2018_1_yr/tall
+mkdir -p data/processed/acs/$1_$2_yr/headers
+mkdir -p data/processed/acs/$1_$2_yr/combined
+mkdir -p data/processed/acs/$1_$2_yr/tall
 
-# download the 2018 1-year ACS
-# TODO: use command line parameters
-curl https://www2.census.gov/programs-surveys/acs/summary_file/2018/data/1_year_entire_sf/All_Geographies.zip > data/raw/acs/2018_1_yr.zip
-curl https://www2.census.gov/programs-surveys/acs/summary_file/2018/data/2018_1yr_Summary_FileTemplates.zip  > data/raw/acs/2018_1_yr_templates.zip
+# try the formats for the 1 and 5 yr ACS files then fail
 
-unzip -j -d data/raw/acs/2018_1_yr/data data/raw/acs/2018_1_yr.zip
-unzip -j -d data/raw/acs/2018_1_yr/templates data/raw/acs/2018_1_yr_templates.zip
+echo "Check for /All_Geographies.zip"
+has_all_zip=`curl --silent --head https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies.zip|head -n 1`
+
+echo "Check for /All_Geographies.tar"
+has_all_tar=`curl --silent --head https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies.tar|head -n 1`
+
+
+
+echo "Check for /All_Geographies_Not_Tracts_Block_Groups.zip"
+has_no_tracts_zip=`curl --silent --head https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.zip|head -n 1`
+
+echo "Check for /All_Geographies_Not_Tracts_Block_Groups.tar"
+#has_no_tracts_tar=`curl --silent --head https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.tar|head -n 1`
+
+echo $has_no_tracts_tar
+
+if [[ $has_no_tracts_zip == *"200"* ]]; then
+    echo "downloading no_tracts zipfile"
+    curl https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.zip > data/raw/acs/$1_$2_yr.zip 
+    unzip -j -d data/raw/acs/$1_$2_yr/data data/raw/acs/$1_$2_yr.zip
+
+elif [[ $has_no_tracts_tar == *"200"* ]]; then
+	echo "downloading no_tracts tarfile"
+	 curl https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies_Not_Tracts_Block_Groups.tar > data/raw/acs/$1_$2_yr.tar
+	 gtar -xf data/raw/acs/$1_$2_yr.tar -C data/raw/acs/$1_$2_yr/data
+elif [[ $has_all_zip == *"200"* ]]; then
+    echo "downloading all geographies zipfile"
+    curl https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies.zip > data/raw/acs/$1_$2_yr.zip
+    unzip -j -d data/raw/acs/$1_$2_yr/data data/raw/acs/$1_$2_yr.zip
+elif [[ $has_all_tar == *"200"* ]]; then
+	echo "downloading all geographies tarfile"
+	curl https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$2_year_entire_sf/All_Geographies.tar > data/raw/acs/$1_$2_yr.tar
+       gtar -xf data/raw/acs/$1_$2_yr.tar -C data/raw/acs/$1_$2_yr/data
+else
+	echo "can't find data"
+	exit
+fi
+	
+	
+curl https://www2.census.gov/programs-surveys/acs/summary_file/$1/data/$1_$2yr_Summary_FileTemplates.zip  > data/raw/acs/$1_$2_yr_templates.zip
+
+#unzip -j -d data/raw/acs/$1_$2_yr/data data/raw/acs/$1_$2_yr.zip
+unzip -j -d data/raw/acs/$1_$2_yr/templates data/raw/acs/$1_$2_yr_templates.zip
