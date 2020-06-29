adec7b19a000c878d4469c10f5c0e1d93fdc77d5
Daniel Lathrop <daniel.lathrop@gmail.com>
2020-06-29 13:03:28 -0700

    remove mysql migration

--- create_mysql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-
-const createTableQuery = `create table if not exists geographies
-	(id primary key auto_increment not null,
-		file_id varchar(10),
-		file varchar(255),
-		state varchar(2), 
-		logrecno varchar(255),
-
